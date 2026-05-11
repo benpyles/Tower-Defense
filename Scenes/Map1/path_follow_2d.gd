6e2444c554fc728bc08aae387a468fb6ed18c31e
benpyles <benpyles118@gmail.com>
2026-05-10 21:28:09 -0400

    deletes obstacles at the end of track now

--- Scenes/Map1/path_follow_2d.gd
+++ Scenes/Map1/path_follow_2d.gd
@@ -3,4 +3,10 @@ extends PathFollow2D
 @export var speed: float = 150.0
 
 func _process(delta: float) -> void:
+	# Move the enemy forward
 	progress += speed * delta
+	
+	# Check if the enemy has reached 100% of the path
+	if progress_ratio >= 1.0:
+		# Delete the enemy from the game to free up memory
+		queue_free()
